c5edbd5eb54f97347c1597edc49b4fddf51b8133
aryanverma2601 <aryanv380@gmail.com>
2025-08-26 12:07:24 +0530

    Compltion of backend

---
+++ server/server.js
@@ -0,0 +1,61 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoute.js";
+import { Server } from "socket.io";
+
+//Creating Express app and Http server
+const app = express();
+const server = http.createServer(app);
+
+//Initialize socket.io server
+
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+//Store online users
+
+export const userSocketMap = {}; // {userId: sockerId}
+
+// Socket.io connection handler
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
+
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // Emit online users to all connected clients
+
+  io.emit("getOnineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnineUsers", Object.keys(userSocketMap));
+  });
+});
+
+//Middleware setup
+
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
+
+app.use("/api/status", (req, res) => {
+  res.send("Server is live");
+});
+
+//Route Setup
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter);
+
+//Connect to Mongodb
+await connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log("Server is running on PORT:" + PORT));
